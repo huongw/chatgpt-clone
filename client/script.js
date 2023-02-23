987a36cd799ab24d2eed29db0d215321cd41aa0f
Whitney <designedbywh@gmail.com>
2023-02-23 18:13:41 -0500

    update server url

--- client/script.js
+++ client/script.js
@@ -77,7 +77,7 @@ const handleSubmit = async (e) => {
 
   // fetch data from server
 
-  const response = await fetch("http://localhost:5000/", {
+  const response = await fetch("https://chatgpt-clone-tswt.onrender.com", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
